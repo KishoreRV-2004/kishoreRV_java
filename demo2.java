1255f1a40b4b17cf26b4984638e5914bfe86fe47
KishoreRV-2004 <rameshkishore2004@gmail.com>
2026-04-09 19:06:06 +0530

    alterated

--- demo2.java
+++ demo2.java
@@ -1,8 +1,17 @@
 class demo2
 {
 
+    void sum()
+    {
+        int a=20;
+        int b=20;
+        System.out.print(a+b);
+    }
+
+
     public static void main(String args[])
     {
-      
+      demo2 server=new demo2();
+      server.sum();
     }
 }
